9c83898dd6d490e425a346dc076b2dcd3f280d91
p0lbang <dayag.vanpaul@gmail.com>
2023-07-10 13:32:54 +0800

    feat: update initial text

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(<h2>Hello World! TypeScript + React + Electron</h2>, document.body);
 }
 
 render();
