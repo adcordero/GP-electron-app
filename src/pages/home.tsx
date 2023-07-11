5e2b911c54830f1c85e3bf3d1699c6c639bad5a5
adcordero <adcordero@gmail.com>
2023-07-11 15:14:58 +0800

    FEAT: onChange handlers + buttons

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import "./home.css"
 import { Row, Col, Button } from "react-bootstrap";
+// import logo from "./images/GP_logomark yellow.png";
+
+// import { Password } from 'primereact/password';
+        
 
 function Home(){
 
     const [firstName, setFirstName]= useState<string>("");
     const [lastName, setLastName]= useState<string>("");
+    // const [lastName, setLastName]= useState<string>("");
+    const [email, setEmail]= useState<string>("");
+    const [Password, setPassword]= useState<string>("");
+    const [retypePass, setRetypePass]= useState<string>("");
+    // const []
+
+    // const [showPass, setShowPass] = useState<boolean>(false);
+    // const [showRetype, setShowRetype] = useState<boolean>(false);
+
 
     const typeFirstName = (e:string) => {
         setFirstName(e);
@@ -15,58 +28,84 @@ function Home(){
         setLastName(e);
     }
 
+    const typeEmail = (e:string) => {
+        setEmail(e);
+    }
+
+    const typePassword = (e:string) => {
+        setPassword(e);
+    }
+
+    const retypePassword = (e:string) => {
+        setRetypePass(e);
+    }
+
+    // const handleShowPassword = () => {
+
+    // }
+
+    const resetForm = () => {
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+        setRetypePass('');
+    }
+
     return (
         <div className="body">
             
             <div className="header">
                 <div>
-                    {/* <img></img> */}
+                    {/* <img src={require("./images/GP_logomark yellow.png")}></img> */}
                 </div>
                 <div>
                     <h1 className="brand">GAMER POINTS<span className="tm">™</span></h1>
                     <h3 className="heading">REGISTRATION</h3>
                 </div>
 
-                <div className="forms">
+                <form className="forms">
                     <div className="name">
                         <div>
-                            <p className="half-tag">First Name</p>
-                            <input type="text" className="cont" onChange={(e) => typeFirstName(e.target.value)}></input>
+                            <label className="half-tag">First Name</label>
+                            <input type="text" className="cont" onChange={(e) => typeFirstName(e.target.value)} value={firstName}></input>
                         </div>
 
                         <div>
-                            <p className="half-tag">Last Name</p>
-                            <input type="text" className="cont" onChange={(e) => typeLastName(e.target.value)}></input>
+                            <label className="half-tag">Last Name</label>
+                            <input type="text" className="cont" onChange={(e) => typeLastName(e.target.value)} value={lastName}></input>
                         </div>
                     </div>
 
-                    <div className="birthday">
-                        <p className="tag">Birthday</p>
-                        <input type="text" className="cont" style={{width: '730px'}}></input>
+                    <div>
+                        <label className="tag">Birthday</label>
+                        <input type="text" className="cont" style={{width: '728px'}}></input>
                     </div>
 
-                    <div className="birthday">
-                        <p className="tag" style={{left: '-340px'}}>Email</p>
-                        <input type="text" className="cont" style={{width: '730px'}}></input>
+                    <div>
+                        <label className="tag" style={{left: '-340px'}}>Email</label>
+                        <input type="text" className="cont" style={{width: '728px'}} onChange={(e) => typeEmail(e.target.value)} value={email}></input>
                     </div>
 
-                    <div className="name">
+                    <div className="password">
                         <div>
-                            <p className="half-tag" style={{left: '-145px'}}>Password</p>
-                            <input type="text" className="cont"></input>
+                            <label className="half-tag" style={{left: '-145px'}}>Password</label>
+                            <input type="text" className="cont" onChange={(e) => typePassword(e.target.value)} value={Password}></input>
                         </div>
 
                         <div>
-                            <p className="half-tag" style={{left: '-120px'}}>Password Retype</p>
-                            <input type="text" className="cont"></input>
+                            <label className="half-tag" style={{left: '-120px'}}>Password Retype</label>
+                            <input type="password" className="pass-cont" onChange={(e) => retypePassword(e.target.value)} value={retypePass}></input>
                         </div>
                     </div>
-                </div>
+                    
+                    <div className="buttons">
+                        <button className="bttn" onClick={() => resetForm()}>Cancel</button>
+                        <button className="bttn" style={{backgroundColor: '#F2CB05'}} type="submit">Sign Up</button>
+                    </div>
+                </form>
 
-                <div className="buttons">
-                    <Button className="bttn">Cancel</Button>
-                    <Button className="bttn" style={{backgroundColor: '#F2CB05'}}>Sign Up</Button>
-                </div>
+                
             </div>
 
         </div>
