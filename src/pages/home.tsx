38259aeb777ed299ddd569589818408630afbf7c
Van Paul Dayag <vcdayag@up.edu.ph>
2023-07-11 13:19:39 +0800

    feat: add basic form structure

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Home(){
-    return <h2>Hello World! TypeScript + React + Electron</h2>;
+    return <div>
+        <h2>Registration Form</h2>
+        <form className="flex flex-col" action="">
+            <input type="text" name="First Name" id="firstname" />
+            <input type="text" name="Last Name" id="lastname" />
+            <input type="date" name="Birthday" id="birthday" />
+            <input type="text" name="Email" id="email" />
+            <input type="text" name="Password" id="password" />
+            <input type="button" value="Submit" />
+        </form>
+    </div>
 };
 
 export default Home;
