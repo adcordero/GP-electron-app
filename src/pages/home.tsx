a6f1fcb6e0c1ac9ceb2f67394ab4fe80b591c711
Danielle Cordero <84511937+adcordero@users.noreply.github.com>
2023-07-12 16:35:03 +0800

    feat: sign up navigation (#13)
    * modify: sign in base done
    
    * chore: finalize design
    
    * feat: sign up button working
    
    ---------
    
    Co-authored-by: adcordero <adcordero@gmail.com>
    Co-authored-by: vcdayag <100204084+vcdayag@users.noreply.github.com>

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import home_bg from "./images/landing.png";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Home Page</h2>
-      <h3>Welcome!</h3>
+    <div style={{backgroundImage: `url(${home_bg})`}}>
+      {/* <h2>Home Page</h2>
+      <h3>Welcome!</h3> */}
+
     </div>
   );
 }
