034f077beb4cea3d15ab4669037b20a1e7eb54a7
adcordero <adcordero@gmail.com>
2023-07-12 14:46:18 +0800

    modify: sign in base done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,34 @@ export default {
   theme: {
     extend: {
       colors: {
-        "brand-primary": "#F2CB05",
+        'white': {
+          100: '#FFFFFF',
+          200: '#F9F9F9',
+          300: '#CECECE',
+          400: '#B5B5B5',
+          500: '#969696',
+        },
+        'black': {
+          100: '#3F3F3F',
+          200: '#2D2D2D',
+          300: '#161616',
+          400: '#0F0F0F',
+          500: '#070707',
+        },
+        'yellow': {
+          100: '#FFE669',
+          200: '#FFE24F',
+          300: '#FFDD32',
+          400: '#FDD716',
+          500: '#F2CB05',
+        }
+      },
+      fontFamily: {
+        'albert-sans': '"Albert Sans"',
+        'poppins': '"Poppins"',
       },
     },
+    
   },
   variants: {},
   plugins: [],
