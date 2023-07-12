f91709da9f93118207266fda0d559c923b9b51ff
Van Paul Dayag <vcdayag@up.edu.ph>
2023-07-12 10:40:57 +0800

    feat: add sign in button

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -5,6 +5,7 @@ export default function Landing() {
   return (
     <div>
       <h2>Landing Page</h2>
+      <Link to="/signin">Sign In</Link>
       <Link to="/signup">Sign Up</Link>
     </div>
   );
