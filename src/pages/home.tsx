1c4a0fedb698ab16d949036ca1f42eb646cd3f7c
Van Paul Dayag <vcdayag@up.edu.ph>
2023-07-14 11:24:07 +0800

    feat: use ign in homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import home_bg from "./images/landing.png";
+import { User } from "../backend/APITypes";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -8,11 +9,15 @@ export default function Home() {
     window.sessionStorage.getItem("token")
   );
 
+  const [userInfo] = useState<User>(
+    JSON.parse(window.sessionStorage.getItem("user"))
+  );
+
   return (
     <div className="relative">
       <img src={home_bg} alt="GP coins" className="h-overall w-full" />
       <p className="absolute text-white-100 top-7 right-7 text-5xl font-poppins text-right font-semibold">
-        WELCOME, <br /> <span className="text-6xl">{username}!</span>
+        WELCOME, <br /> <span className="text-6xl">{userInfo.ign}!</span>
       </p>
 
       <button
